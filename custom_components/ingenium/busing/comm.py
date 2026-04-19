9e8cfd441e03a966f438e2373d980713cfd9c89c
jaysphoto <mail@jaysphoto.me>
2026-04-19 11:20:24 +0000

    BUSing library improvements
    - Use IngeniumBUSingDatagram class for encoding/decode bus datagrams
    - Added retries class parameter for retrying operations with IOErrors
    - Fixed duplicate \'Connected to:\' messages with multiple Coroutines

--- custom_components/ingenium/busing/comm.py
+++ custom_components/ingenium/busing/comm.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 
-from struct import unpack
+from struct import pack, unpack
 from typing import List
 
 _LOGGER = logging.getLogger(__name__)
@@ -13,18 +13,19 @@ class IngeniumBUSingCommunication:
     DEFAULT_PORT = 12347
     RESPONSE_TIMEOUT = 15
     RECONNECT_DELAY = 5
+    RECONNECT_RETRIES = 5
 
     def __init__(
-        self,
-        host: str,
-        port: int = DEFAULT_PORT,
-        decoder: "IngeniumBUSingDecoder" = None,
+        self, host: str, port: int = DEFAULT_PORT, retries: int = RECONNECT_RETRIES
     ):
         self._host = host
         self._port = port
-        self._decoder = decoder or IngeniumBUSingDecoder()
+        self._reader = None
+        self._writer = None
+        self._retries = retries
 
     async def listener(self, callback=None):
+        retries = self._retries
         """TCP client task that connects to device and logs incoming data in hex."""
         while True:
             try:
@@ -35,51 +36,54 @@ class IngeniumBUSingCommunication:
                         d = await self._read_messages()
 
                         if d is None:
-                            break
+                            raise IOError("Lost connection")
 
-                        if callback is not None:
+                        # Connection is alive - reset retries
+                        retries = self._retries
+                        if not callback is None:
                             callback(d)
                 finally:
                     await self._close_connection()
             except asyncio.CancelledError:
                 _LOGGER.info("Listener cancelled, closed connection")
                 break
-            except Exception as e:
-                _LOGGER.error(
+            except IOError as e:
+                if retries == 0:
+                    _LOGGER.warning(
+                        f"Connection failed: {e}, giving up after {self._retries} tries"
+                    )
+                    break
+
+                _LOGGER.warning(
                     f"Connection failed: {e}, retrying in {self.RECONNECT_DELAY} seconds"
                 )
+                retries = retries - 1
                 await asyncio.sleep(self.RECONNECT_DELAY)
 
     async def send_message(
-        self, command: int, origin: int, destination: int, data1: int, data2: int
+        self, command: int, _origin: int, destination: int, data1: int, data2: int
     ):
-        """Send a message to the Ingenium server."""
-        if not hasattr(self, "_writer") or self._writer is None:
-            _LOGGER.error("Not connected to server, cannot send message")
-            return
-
-        # Construct the message according to the protocol
-        message = bytearray(7)
-        message[0] = b"\xff"  # Start byte
-        message[1] = b"\xff"  # Start byte
-        message[2] = command & 0xFF
-        message[3] = (destination >> 8) & 0xFF
-        message[4] = (destination) & 0xFF
-        message[5] = data1 & 0xFF
-        message[6] = data2 & 0xFF
+        """Send structured Ingenium BUSing message."""
+        origin = 0xFFFF  # Start bytes
+        message = IngeniumBUSingDatagram.encode(
+            origin, command, destination, data1, data2
+        )
 
         return await self.send_message_raw(message)
 
     async def send_message_raw(self, message: bytearray | bytes):
+        """Send raw Ingenium BUSing message."""
         await self._open_connection()
+
         try:
-            _LOGGER.info("Sending: %s", message.hex())
+            _LOGGER.info("Sending raw message: %s", message.hex())
             self._writer.write(message)
             return await self._writer.drain()
         except Exception as e:
             _LOGGER.error("Failed to send message: %s", e)
 
     async def await_response(self, timeout=RESPONSE_TIMEOUT) -> dict | None:
+        """Wait for a matching response, RequestReply pattern implementation."""
         start_t = asyncio.get_event_loop().time()
         while timeout > 0:
             _LOGGER.debug(f"Waiting for response message (timeout={timeout})...")
@@ -95,11 +99,11 @@ class IngeniumBUSingCommunication:
             timeout = timeout - (asyncio.get_event_loop().time() - start_t)
 
     async def _open_connection(self):
-        if not hasattr(self, "_reader") or self._reader is None:
+        if self._reader is None:
             self._reader, self._writer = await asyncio.open_connection(
                 self._host, self._port
             )
-        _LOGGER.info("Connected to %s:%d", self._host, self._port)
+            _LOGGER.info("Connected to %s:%d", self._host, self._port)
 
     async def _close_connection(self):
         if not self._writer.is_closing():
@@ -109,39 +113,39 @@ class IngeniumBUSingCommunication:
         self._reader = self._writer = None
 
     async def _read_messages(self) -> List[dict] | None:
-        if self._reader.at_eof():
-            return None
-
-        data = await self._reader.read(1024)
+        # Read up to 100 datagrams at the time
+        data = await self._reader.read(9 * 100)
         if not data:
             return None
 
-        decoded_messages = self._decoder.decode_message(data)
+        decoded_messages = IngeniumBUSingDatagram.decode(data)
 
         [_LOGGER.debug("Received datagram: %s", msg) for msg in decoded_messages]
 
         return decoded_messages
 
 
-class IngeniumBUSingDecoder:
-    """
-    Class to decode BUSing protocol messages from Ingenium server.
-    Messages on the bus are sent in 9-byte frames, for example:
-    Connected to 192.168.xxx.xx:12347
-    Received data: fefe04000bfefe2300
-    Received data: fefe04000b000b0002
-    Received data: fefe04000b000b0113fefe04000b000b0208fefe04000b000b0370
-    """
+class IngeniumBUSingDatagram:
+    def decode(data: bytes) -> List[dict]:
+        """
+        Decode recieved bytes into a list of structured dictionary.
 
-    def decode_message(self, data: bytes) -> List[dict]:
-        """Decode recieved bytes into a list of structured dictionary."""
+        Messages on the bus are sent in 9-byte frames, for example:
+
+        Connected to 192.168.xxx.xx:12347
+        Received data: fefe04000bfefe2300
+        Received data: fefe04000b000b0002
+        Received data: fefe04000b000b0113fefe04000b000b0208fefe04000b000b0370
+        """
         messages = []
         offset = 0
         while offset < len(data):
             # Datagrams are each 9 bytes long, so we read in chunks of 9 bytes
             dg = data[offset : offset + 9]
             if len(dg) < 9:
-                _LOGGER.warning("Incomplete datagram received: %s", dg.hex())
+                raise IngeniumBUSingDataInvalid(
+                    f"Incomplete datagram received: {dg.hex()}"
+                )
                 break
             cmd, origin, destination, data1, data2 = unpack(">xxbHHBB", dg)
             decoded = {
@@ -157,3 +161,11 @@ class IngeniumBUSingDecoder:
             offset += 9
 
         return messages
+
+    def encode(origin, command, destination, data1, data2) -> bytearray:
+        # Construct the message according to the protocol
+        return pack(">HHbbb", origin, destination, command, data1, data2)
+
+
+class IngeniumBUSingDataInvalid(IOError):
+    pass
